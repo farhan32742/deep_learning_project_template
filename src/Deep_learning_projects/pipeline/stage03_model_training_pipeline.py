5d03596f02442620e2310df604767e8f1e3ec6bd
Farhan <farhan32742@example.com>
2025-12-10 23:42:44 +0500

    integrating dvc

--- src/Deep_learning_projects/pipeline/stage03_model_training_pipeline.py
+++ src/Deep_learning_projects/pipeline/stage03_model_training_pipeline.py
@@ -1,7 +1,10 @@
 from Deep_learning_projects.config.configuration import ConfigurationManager
 from Deep_learning_projects.components.model_training import Training
 from Deep_learning_projects.utils import log
+import os
+from dotenv import load_dotenv, find_dotenv
 
+load_dotenv(find_dotenv(), override=True)
 STAGE_NAME = "Training"
 
 
@@ -28,6 +31,13 @@ if __name__ == '__main__':
     try:
         log.info(f"*******************")
         log.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        
+        # Debug Check for DVC
+        if not os.getenv("MLFLOW_TRACKING_PASSWORD"):
+            log.warning("⚠️  DVC Warning: MLFLOW_TRACKING_PASSWORD not found in environment!")
+        else:
+            log.info("✅ DVC successfully loaded .env credentials.")
+
         obj = ModelTrainingPipeline()
         obj.main()
         log.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
